c63943e47dff1d1b1a5f969b4f18835300d87029
dvverma03 <dvverma9211@gmail.com>
2024-08-09 19:58:28 +0530

    update frontend url

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000","https://pocket-notes-frontend-navy.vercel.app"],
   credentials: true
 }));
 
